e11cadc86ca5fdd4007cb4c2d63e13f6fb4e4ae1
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-08-01 16:18:40 -0700

    added disconnect message

--- server.js
+++ server.js
@@ -11,9 +11,24 @@ app.use(express.static(__dirname + '/public'));
 var clientInfo = {};
 
 io.on('connection',function(socket){
+
+	socket.on('disconnect',function(){
+		var userData = clientInfo[socket.id];
+
+		if(typeof userData !=='undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message',{
+				name:'System',
+				text:userData.name+ " has left",
+				timestamp:moment.valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+
+	});
 	
 	socket.on('joinRoom',function(req){
-		
+
 		clientInfo[socket.id] = req;
 
 		socket.join(req.room);
